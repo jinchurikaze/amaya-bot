10e733804d082df8edc731dfbcc591ee081a17d4
Stephanie Arjona <stephanierosebornilla.arjona24@bicol-u.edu.ph>
2026-03-07 17:31:44 +0800

    debug mongourl

--- database.js
+++ database.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+console.log("🔍 MONGO_URI:", process.env.MONGO_URI); // temp debug line
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch((error) => {
