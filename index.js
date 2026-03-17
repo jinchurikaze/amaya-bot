f94035575562b381fb628c57a6a972fe13e79735
Stephanie Arjona <stephanierosebornilla.arjona24@bicol-u.edu.ph>
2026-03-17 21:26:51 +0800

    added guild members

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
   ],
 });
 
